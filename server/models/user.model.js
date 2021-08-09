9429dd3b557de3a48669ffdacede31716e87932a
Hoda Shobaki <shobkihoda@gmail.com>
2021-08-09 16:03:31 +0300

    model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,39 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-	name: String,
-	age: Number
-});
+	name: {
+		type: String,
+		required: [true, "Name is required"]
+	  },
+	  email: {
+		type: String,
+		required: [true, "Email is required"],
+		validate: {
+			validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+			message: "Please enter a valid email"
+		  }
+	  },
+	  password: {
+		type: String,
+		required: [true, "Password is required"],
+		minlength: [8, "Password must be 8 characters or longer"]
+	  },
+	 
+	  address:{
+		  type:String,
+		  require:[true,"location is a required field"]
+	  },
+	}, {timestamps: true});
 
 const User = mongoose.model("User", UserSchema);
+UserSchema.virtual('confirmPassword')
+  .get( () => this._confirmPassword )
+  .set( value => this._confirmPassword = value );
+  UserSchema.pre('validate', function(next) {
+	if (this.password !== this.confirmPassword) {
+	  this.invalidate('confirmPassword', 'Password must match confirm password');
+	}
+	next();
+  });
 
 module.exports = User;
