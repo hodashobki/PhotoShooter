e59aa8dfd555dda95b4dcd7df4790b2c4994de6f
ayat jamhour <engayatjamhour@gmail.com>
2021-08-10 16:25:41 +0300

    user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,13 @@
 const UserController = require("../controllers/user.controller");
 
 module.exports = app => {
+
   app.get("/api/users/", UserController.findAllUsers);
   app.get("/api/users/:id", UserController.findOneSingleUser);
   app.put("/api/users/update/:id", UserController.updateExistingUser);
   app.post("/api/users/new", UserController.createNewUser);
   app.delete("/api/users/delete/:id", UserController.deleteAnExistingUser);
+  //login and reg
+  app.post("/login", UserController.loginUser);
+  app.post("/register", UserController.regUser);
 };
