c6db2402589ff91cc72d6962eeaeb0287319281e
Omar Alhaddar <omaralhaddar11@gmail.com>
2021-08-11 12:52:38 +0300

    header

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,18 @@ import Chat from './views/Chat'
 import PhotoDetails from './views/PhotoDetails'
 import Header from './components/Header'
 import Footer from './components/Footer'
-<<<<<<< HEAD
-=======
-import Logo from './components/Logo';
-import CreatComment from './views/CreatComment';
->>>>>>> 95bb1ef407ee198537c9fb4effae501fda622faa
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CreatePhoto/>
-      <CreatComment/>
       <Router >
       <Main path ="/"/>
       <Login path = "/login"/>
       <Register path = "/register"/>
       <PhotoDetails path = "/photo/:id"/>   
-      {/* <CreatePhoto path ="/createPhoto" /> */}
+      <CreatePhoto path ="/createPhoto" />
       <PhotographerDetail path= "/photographer/:id"/>
       <Chat path= "/chat"/>
 
