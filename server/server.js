da29a38864c92ab4917ef86ddb7e0c1b0a652956
Hoda Shobaki <shobkihoda@gmail.com>
2021-08-12 08:51:18 +0300

    F

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors=require("cors")
 app.use(cors());
-// const cookieParser = require('cookie-parser');
+
 
 // This will fire our mongoose.connect statement to initialize our database connection
 require("./config/mongoose.config");
@@ -13,7 +13,6 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 // This is where we import the users routes function from our user.routes.js file
 const AllMyUserRoutes = require("./routes/user.routes");
 AllMyUserRoutes(app);
-// require("./routes/phot.routes")(app);
 const AllPhots=require("./routes/phot.routes");
 AllPhots(app);
 require("./routes/comment.routes")(app);
