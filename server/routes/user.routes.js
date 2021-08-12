a918814e9d6906a8eb63cfd9054567620ff91dad
ayat jamhour <engayatjamhour@gmail.com>
2021-08-12 16:59:07 +0300

    user router

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -7,7 +7,8 @@ module.exports = app => {
   app.put("/api/users/update/:id", UserController.updateExistingUser);
   app.post("/api/register",UserController.regUser);
   app.delete("/api/users/delete/:id", UserController.deleteAnExistingUser);
-  app.post("/api/login",UserController.loginUser);
+  app.post("/api/login", UserController.login);
+  app.get("/api/logout", UserController.logout);
     //login and reg
   // app.post("/login", UserController.loginUser);
   // app.post("/register", UserController.regUser);
