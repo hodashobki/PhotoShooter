6641b0135a6708098f514dcfcceb406f7ed66972
Hoda Shobaki <shobkihoda@gmail.com>
2021-08-13 17:54:34 +0300

    socket updates

--- server/socket/router.js
+++ server/socket/router.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('You see server is up running');
+    res.send('You server is up running');
 });
 
 module.exports=router;
