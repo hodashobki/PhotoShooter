d12b2e06052ca71c491a8d0cf1fac4875647ef38
Hoda Shobaki <shobkihoda@gmail.com>
2021-08-13 20:30:59 +0300

    final socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const socketio=require('socket.io');
 const http =require('http');
-const router = require("./router");
 const cors = require('cors');
-const {addUser,removeUser,getUser,getUserInRoom}=require('./users')
+const {addUser,removeUser,getUser,getUsersInRoom}=require('./users')
+const router = require("./router");
 
 const PORT=process.env.PORT ||5000;
 
@@ -17,17 +17,15 @@ app.use(router);
 
 
 io.on('connection',(socket)=>{
-    console.log('We have a new connection!!!');
-   
     socket.on('join',({name,room},callback)=>{
         const {error,user}=addUser({id:socket.id,name,room});
-        socket.join(user.room);// added unsure
+       
         if(error) return callback(error);
-        socket.emit('message',{user:'admin',text:`${user.name},welcom to the room,${user.room}`});
+        socket.join(user.room);// added unsure
+
+        socket.emit('message',{user:'admin',text:`${user.name},welcom to room,${user.room}`});
         socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name} ,has joined`});
         
-        socket.join(user.room);
-
         io.to(user.room).emit('roomDate',{room:user.room,users:getUsersInRoom(user.room)});
 
         callback();
